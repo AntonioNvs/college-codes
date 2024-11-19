6cdf241234ccc1296a5e6c889af7d49a29368746
AntonioNvs <tonim2612@gmail.com>
2024-11-18 22:49:07 -0300

    VPL 9

---
+++ SEMESTRES/6/COMPILADORES/Funções Recursivas/Lexer.py
@@ -0,0 +1,207 @@
+import re
+import sys
+import enum
+
+
+class Token:
+    """
+    This class contains the definition of Tokens. A token has two fields: its
+    text and its kind. The "kind" of a token is a constant that identifies it
+    uniquely. See the TokenType to know the possible identifiers (if you want).
+    You don't need to change this class.
+    """
+    def __init__(self, tokenText, tokenKind):
+        # The token's actual text. Used for identifiers, strings, and numbers.
+        self.text = tokenText
+        # The TokenType that this token is classified as.
+        self.kind = tokenKind
+
+
+class TokenType(enum.Enum):
+    """
+    These are the possible tokens. You don't need to change this class at all.
+    """
+
+    EOF = -1  # End of file
+    NLN = 0  # New line
+    WSP = 1  # White Space
+    COM = 2  # Comment
+    NUM = 3  # Number (integers)
+    STR = 4  # Strings
+    TRU = 5  # The constant true
+    FLS = 6  # The constant false
+    VAR = 7  # An identifier
+    LET = 8  # The 'let' of the let expression
+    INX = 9  # The 'in' of the let expression
+    END = 10  # The 'end' of the let expression
+    EQL = 201  # x = y
+    ADD = 202  # x + y
+    SUB = 203  # x - y
+    MUL = 204  # x * y
+    DIV = 205  # x div y
+    LEQ = 206  # x <= y
+    LTH = 207  # x < y
+    NEG = 208  # ~x
+    NOT = 209  # not x
+    LPR = 210  # (
+    RPR = 211  # )
+    VAL = 212  # The 'val' declaration
+    ORX = 213  # x or y
+    AND = 214  # x and y
+    IFX = 215  # The 'if' of a conditional expression
+    THN = 216  # The 'then' of a conditional expression
+    ELS = 217  # The 'else' of a conditional expression
+    FNX = 218  # The 'fn' that declares an anonymous function
+    ARW = 219  # The '=>' that separates the parameter from the body of function
+    FUN = 220  # The 'fun' declaration
+    MOD = 221  # The 'mod' operator
+
+
+class Lexer:
+    
+    def __init__(self, source):
+        """
+        The constructor of the lexer. It receives the string that shall be
+        scanned.
+        TODO: You will need to implement this method.
+        """
+        self.source = source
+
+    def tokens(self):
+        """
+        This method is a token generator: it converts the string encapsulated
+        into this object into a sequence of Tokens. Notice that this method
+        filters out three kinds of tokens: white-spaces, comments and new lines.
+
+        Examples:
+
+        >>> l = Lexer("1 + 3")
+        >>> [tk.kind for tk in l.tokens()]
+        [<TokenType.NUM: 3>, <TokenType.ADD: 202>, <TokenType.NUM: 3>]
+
+        >>> l = Lexer('1 * 2\\n')
+        >>> [tk.kind for tk in l.tokens()]
+        [<TokenType.NUM: 3>, <TokenType.MUL: 204>, <TokenType.NUM: 3>]
+
+        >>> l = Lexer('1 * 2 -- 3\\n')
+        >>> [tk.kind for tk in l.tokens()]
+        [<TokenType.NUM: 3>, <TokenType.MUL: 204>, <TokenType.NUM: 3>]
+
+        >>> l = Lexer("1 + var")
+        >>> [tk.kind for tk in l.tokens()]
+        [<TokenType.NUM: 3>, <TokenType.ADD: 202>, <TokenType.VAR: 7>]
+
+        >>> l = Lexer("let val v = 2 in v end")
+        >>> [tk.kind.name for tk in l.tokens()]
+        ['LET', 'VAL', 'VAR', 'EQL', 'NUM', 'INX', 'VAR', 'END']
+
+        >>> l = Lexer("fn x => x div 1")
+        >>> [tk.kind.name for tk in l.tokens()]
+        ['FNX', 'VAR', 'ARW', 'VAR', 'DIV', 'NUM']
+
+        >>> l = Lexer("fun inc a = a mod 2")
+        >>> [tk.kind.name for tk in l.tokens()]
+        ['FUN', 'VAR', 'VAR', 'EQL', 'VAR', 'MOD', 'NUM']
+        """
+        token = self.getToken()
+        while token.kind != TokenType.EOF:
+            if token.kind != TokenType.WSP and token.kind != TokenType.COM \
+                    and token.kind != TokenType.NLN:
+                yield token
+            token = self.getToken()
+
+    def getToken(self):
+        if len(self.source.strip()) == 0:
+            return Token("", TokenType.EOF)
+
+        self.source = self.source.lstrip()
+
+        if self.source[:2] == "--":
+            idx = self.source.find("\n")
+            if idx != -1:
+                self.source = self.source[idx+1:]
+            else:
+                self.source = str()
+            return Token("", TokenType.COM)
+        
+        if self.source[:2] == "(*":
+            idx = self.source.find("*)")
+            if idx == -1:
+                raise Exception("Comment was not finished.")
+            else:
+                self.source = self.source[idx+2:]
+                return Token("", TokenType.COM)
+
+        number_pattern = r"^\d+"
+        identifier_pattern = r"^[a-zA-Z_]\w*"
+        operator_pattern = r"^(\+|\-|\*|<=|=>|<|=|~|\(|\))"
+        keyword_patterns = {
+            "let": TokenType.LET,
+            "in": TokenType.INX,
+            "end": TokenType.END,
+            "fn": TokenType.FNX,
+            "not": TokenType.NOT,
+            "true": TokenType.TRU,
+            "True": TokenType.TRU,
+            "False": TokenType.FLS,
+            "false": TokenType.FLS,
+            "or": TokenType.ORX,
+            "and": TokenType.AND,
+            "if": TokenType.IFX,
+            "then": TokenType.THN,
+            "else": TokenType.ELS,
+            "val": TokenType.VAL,
+            "fun": TokenType.FUN,
+            "mod": TokenType.MOD,
+            "div": TokenType.DIV,
+        }
+
+        # Check for numbers
+        match = re.match(number_pattern, self.source)
+        if match:
+            number = match.group(0)
+            self.source = self.source[len(number):]
+            return Token(int(number), TokenType.NUM)
+
+        # Check for keywords
+        match = re.match(identifier_pattern, self.source)
+        if match:
+            identifier = match.group(0)
+            self.source = self.source[len(identifier):]
+
+            if identifier in keyword_patterns:
+                token_type = keyword_patterns[identifier]
+                if token_type == TokenType.LET:
+                    self.var_expected = True 
+                return Token(identifier, token_type)
+            else:
+                return Token(identifier, TokenType.VAR)
+
+        # Check for operators
+        match = re.match(operator_pattern, self.source)
+        if match:
+            operator = match.group(0)
+            self.source = self.source[len(operator):]
+            token_map = {
+                "+": TokenType.ADD,
+                "-": TokenType.SUB,
+                "*": TokenType.MUL,
+                "/": TokenType.DIV,
+                "<=": TokenType.LEQ,
+                "<": TokenType.LTH,
+                "=": TokenType.EQL,
+                "~": TokenType.NEG,
+                "(": TokenType.LPR,
+                ")": TokenType.RPR,
+                "=>" : TokenType.ARW
+            }
+            return Token(operator, token_map.get(operator, TokenType.EOF))
+
+        match = re.match(identifier_pattern, self.source)
+        if match:
+            var_name = match.group(0)
+            self.source = self.source[len(var_name):]
+            return Token(var_name, TokenType.VAR)
+
+        
+        raise Exception("Unrecognized semantic.")
---
+++ SEMESTRES/6/COMPILADORES/Funções Recursivas/Visitor.py
@@ -0,0 +1,347 @@
+import sys
+from abc import ABC, abstractmethod
+from Expression import *
+
+
+class Visitor(ABC):
+    """
+    The visitor pattern consists of two abstract classes: the Expression and the
+    Visitor. The Expression class defines on method: 'accept(visitor, args)'.
+    This method takes in an implementation of a visitor, and the arguments that
+    are passed from expression to expression. The Visitor class defines one
+    specific method for each subclass of Expression. Each instance of such a
+    subclasse will invoke the right visiting method.
+    """
+
+    @abstractmethod
+    def visit_var(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_bln(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_num(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_eql(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_and(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_or(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_add(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_sub(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_mul(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_div(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_mod(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_leq(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_lth(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_neg(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_not(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_let(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_ifThenElse(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_fn(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_fun(self, exp, arg):
+        pass
+
+    @abstractmethod
+    def visit_app(self, exp, arg):
+        pass
+
+
+class Function:
+    """
+    This is the class that represents functions. This class lets us distinguish
+    the three types that now exist in the language: numbers, booleans and
+    functions. Notice that the evaluation of an expression can now be a
+    function. For instance:
+
+        >>> f = Fn('v', Mul(Var('v'), Var('v')))
+        >>> ev = EvalVisitor()
+        >>> fval = f.accept(ev, {})
+        >>> type(fval)
+        <class 'Visitor.Function'>
+    """
+
+    def __init__(self, formal, body, env):
+        self.formal = formal
+        self.body = body
+        self.env = env
+
+    def __str__(self):
+        return f"Fn({self.formal})"
+
+
+class RecFunction(Function):
+    """
+    This is the class that represents named functions. The key different between
+    named and anonymous functions is exactly the "name" :)
+
+        >>> f = Fun('f', 'v', Mul(Var('v'), Var('v')))
+        >>> ev = EvalVisitor()
+        >>> fval = f.accept(ev, {})
+        >>> type(fval)
+        <class 'Visitor.RecFunction'>
+    """
+
+    def __init__(self, name, formal, body, env):
+        super().__init__(formal, body, env)
+        self.name = name
+
+    def __str__(self):
+        return f"Fun {self.name}({self.formal})"
+
+
+class EvalVisitor(Visitor):
+    """
+    The EvalVisitor class evaluates logical and arithmetic expressions. The
+    result of evaluating an expression is the value of that expression. The
+    inherited attribute propagated throughout visits is the environment that
+    associates the names of variables with values.
+
+    Examples:
+    >>> e0 = Let('v', Add(Num(40), Num(2)), Mul(Var('v'), Var('v')))
+    >>> e1 = Not(Eql(e0, Num(1764)))
+    >>> ev = EvalVisitor()
+    >>> e1.accept(ev, {})
+    False
+
+    >>> e0 = Let('v', Add(Num(40), Num(2)), Sub(Var('v'), Num(2)))
+    >>> e1 = Lth(e0, Var('x'))
+    >>> ev = EvalVisitor()
+    >>> e1.accept(ev, {'x': 41})
+    True
+
+    >>> e0 = Fn('v', Mul(Var('v'), Var('v')))
+    >>> ev = EvalVisitor()
+    >>> print(e0.accept(ev, {}))
+    Fn(v)
+
+    >>> e0 = Fn('v', Mul(Var('v'), Var('v')))
+    >>> e1 = Add(Num(3), Num(4))
+    >>> e2 = App(e0, e1)
+    >>> ev = EvalVisitor()
+    >>> print(e2.accept(ev, {}))
+    49
+
+    >>> e0 = Fun('f', 'v', Mul(Var('v'), Var('v')))
+    >>> ev = EvalVisitor()
+    >>> print(e0.accept(ev, {}))
+    Fun f(v)
+
+    >>> e0 = Fun('f', 'v', Mul(Var('v'), Var('v')))
+    >>> e1 = Add(Num(3), Num(4))
+    >>> e2 = App(e0, e1)
+    >>> ev = EvalVisitor()
+    >>> print(e2.accept(ev, {}))
+    49
+    """
+
+    def visit_var(self, exp, env):
+        if exp.identifier in env:
+            return env[exp.identifier]
+        else:
+            sys.exit("Def error")
+
+    def visit_bln(self, exp, env):
+        return eval(exp.bln)
+
+    def visit_num(self, exp, env):
+        return int(exp.num)
+
+    def visit_eql(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(val_right):
+            return val_left == val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_and(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(True) and type(val_right) == type(True):
+            return val_left and val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_or(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(True) and type(val_right) == type(True):
+            return val_left or val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_add(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(1) and type(val_right) == type(1):
+            return val_left + val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_sub(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(1) and type(val_right) == type(1):
+            return val_left - val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_mul(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(1) and type(val_right) == type(1):
+            return val_left * val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_div(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(1) and type(val_right) == type(1):
+            return val_left // val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_mod(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(1) and type(val_right) == type(1):
+            return val_left % val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_leq(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(1) and type(val_right) == type(1):
+            return val_left <= val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_lth(self, exp, env):
+        val_left = exp.left.accept(self, env)
+        val_right = exp.right.accept(self, env)
+        if type(val_left) == type(1) and type(val_right) == type(1):
+            return val_left < val_right
+        else:
+            sys.exit("Type error")
+
+    def visit_neg(self, exp, env):
+        val_exp = exp.exp.accept(self, env)
+        if type(val_exp) == type(1):
+            return -val_exp
+        else:
+            sys.exit("Type error")
+
+    def visit_not(self, exp, env):
+        val_exp = exp.exp.accept(self, env)
+        if type(val_exp) == type(True):
+            return not val_exp
+        else:
+            sys.exit("Type error")
+
+    def visit_let(self, exp, env):
+        new_env = dict(env)
+        new_env[exp.identifier] = exp.exp_def.accept(self, env)
+        return exp.exp_body.accept(self, new_env)
+
+    def visit_ifThenElse(self, exp, env):
+        val_cond = exp.cond.accept(self, env)
+        if type(val_cond) == type(True):
+            return exp.e0.accept(self, env) if val_cond else exp.e1.accept(self, env)
+        
+    def visit_fn(self, exp, env):
+        """
+        The evaluation of a function is the function itself. Remember: in our
+        language, functions are values as well. So, now we have four kinds of
+        values: numbers, booleans, anonymous functions and named functions.
+        """
+        return Function(exp.formal, exp.body, env)
+
+    def visit_fun(self, exp, env):
+        """
+        The evaluation of a named function returns a value that is the function
+        itself. However, we use a different type of value: RecFunction. In this
+        way, we have access to the name of the named function (and that's why
+        they are called named functions :).
+        """
+        return RecFunction(exp.name, exp.formal, exp.body, env)
+
+    def visit_app(self, exp, env):
+        """
+        The application of function to actual parameter must contain two cases:
+        1. An anonymous function is applied: (fn x => x + 1) 2
+        2. A named function is applied: f 2, where f is fun f a = a + a
+        The only difference between these two cases is that in the second we
+        must augment the environment with the name of the named function.
+
+        Example:
+        >>> f = Fun('f', 'v', Mul(Var('v'), Var('v')))
+        >>> e0 = Let('f', f, App(Var('f'), Num(2)))
+        >>> ev = EvalVisitor()
+        >>> e0.accept(ev, {})
+        4
+        """
+
+        fval = exp.function.accept(self, env)
+
+        if isinstance(fval, Function):
+            val_param = exp.actual.accept(self, env)
+            new_env = dict(fval.env)
+            new_env[fval.formal] = val_param
+
+            if getattr(fval, "name", None) is not None:
+                new_env[fval.name] = fval
+
+            return fval.body.accept(self, new_env)
+        else:
+            sys.exit("Type error")
+        
